0f8e5b0cc8ea3fcdc050947e817c26c09ad329e8
matheusbsilva <matheusbattista@hotmail.com>
2019-12-04 15:48:57 -0300

    add basic operators on environment

--- plai/environment.py
+++ plai/environment.py
@@ -1,10 +1,31 @@
+import operator as op
+
 from .symbol import Symbol
 
 
 def env(variables=None):
+    env = global_env
+
     if variables:
         if any(not isinstance(key, Symbol) for key in variables):
             raise TypeError('variables must be instance of Symbol')
-        return variables
+        env.update(variables)
+        return env
     else:
-        return {}
+        return env
+
+
+def _make_global_env():
+    dic = {
+        '+': op.add,
+        '-': op.sub,
+        '*': op.mul,
+        '/': op.truediv
+    }
+
+    global_ops = {Symbol(k): v for k, v in dic.items()}
+
+    return global_ops
+
+
+global_env = _make_global_env()
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,6 +1,7 @@
 import pytest
 
 from plai.environment import env
+from plai.environment import global_env
 from plai.symbol import Symbol
 
 
@@ -16,3 +17,7 @@ class TestEnvironment:
     def test_vars_can_only_be_symbols(self):
         with pytest.raises(TypeError):
             env({'x': 1})
+
+    def test_basic_operators_exists_in_default_env(self):
+        assert env() == global_env
+        assert set(env({Symbol('x'): 3})).issuperset(global_env)
