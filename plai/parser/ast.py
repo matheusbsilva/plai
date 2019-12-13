027dea40b635b10b8c00d946edc567379ce32c55
matheusbsilva <matheusbattista@hotmail.com>
2019-12-13 19:11:59 -0300

    [#10]: implement AST __new__ method and print

--- plai/parser/ast.py
+++ plai/parser/ast.py
@@ -1,10 +1,34 @@
+from lark import Token
+
+from plai.symbol import Symbol
+
+
 class AST:
-    def __init__(self, token):
-        self.token = token
-        self.children = []
+    def __new__(cls, token):
+        if isinstance(token, AST):
+            new = token
+        else:
+            if not isinstance(token, (Token, Symbol, AST)):
+                raise TypeError("'%s' must be an instance of Token or Symbol")
+
+            new = super().__new__(cls)
+            new.token = token
+            new.children = []
+
+        return new
 
     def add_child(self, child):
         if not isinstance(child, AST):
             raise TypeError("'%s' must be an instance of AST" % child)
 
         self.children.append(child)
+
+    def __str__(self):
+        return str(self.token)
+
+    def print(self, level=0):
+        rep = level*" " + self.__str__() + "\n"
+        for node in self.children:
+            rep += node.print(level+1)
+
+        return rep
--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,23 +1,50 @@
 import pytest
+
+from lark import Token
+
 from plai.parser import AST
 
 
 class TestAST:
     def test_initialization(self):
-        ast = AST('token')
+        ast = AST(Token('TOKEN', 'token'))
 
         assert ast.token == 'token'
         assert ast.children == []
 
     def test_add_child_method(self):
-        ast = AST('token')
-        child = AST('child')
+        ast = AST(Token('TOKEN', 'token'))
+        child = AST(Token('CHILD', 'child'))
         ast.add_child(child)
 
         assert ast.children[0] == child
 
     def test_add_child_only_receives_instance_of_AST(self):
-        ast = AST('token')
+        ast = AST(Token('TOKEN', 'token'))
 
         with pytest.raises(TypeError):
             ast.add_child('child')
+
+    def test_raises_typeerror_for_unvalid_token_type(self):
+        with pytest.raises(TypeError):
+            AST('token')
+
+    def test_initialization_with_ast(self):
+        """
+        When initialized with an AST instance it must return that instance
+        """
+
+        ast = AST(Token('X', 'x'))
+
+        assert AST(ast) == ast
+
+    def test_print(self):
+        root = AST(Token('ROOT', 'root'))
+        child1 = AST(Token('CHILD1', 'child1'))
+        child2 = AST(Token('CHILD2', 'child2'))
+
+        child1.add_child(child2)
+        root.add_child(child1)
+
+        print_result = "root\n child1\n  child2\n"
+        assert root.print() == print_result
