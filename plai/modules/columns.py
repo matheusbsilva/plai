a7d68d6c763a3464f8b5a8924db2673b9e6211c6
Matheus Batista <matheusbattista@hotmail.com>
2021-09-25 12:49:13 -0300

    Implement add operation for column

--- plai/modules/columns.py
+++ plai/modules/columns.py
@@ -1,6 +1,17 @@
+import operator as op
+
+
 class Col:
-    def __init__(self, name):
+    def __init__(self, name, dataframe):
+        self.dataframe = dataframe
         self.name = name
 
-    def __call__(self, dataframe):
-        return dataframe[self.name]
+    def __call__(self):
+        return self.dataframe[self.name]
+
+    def operation(self, row, operation, *sargs):
+        row[self.name] = operation(row[self.name], *sargs)
+        return row
+
+    def __add__(self, right):
+        return self.dataframe.apply(self.operation, axis=1, args=(op.add, right))
