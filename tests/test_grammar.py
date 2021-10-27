f16e1727ae1970885c27b5e88cb4a64577bb9d90
Matheus Batista <matheusbattista@hotmail.com>
2021-10-26 22:06:14 -0300

    Create output stmt for pipeline

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -271,3 +271,34 @@ pipeline(bar):
             [Symbol.COLUMN, 'col'],
             [Symbol.COLUMN, 'col2']
         ]
+
+    def test_output_stmt_multiple_line_for_pipeline(self):
+        src = """
+pipeline(df) -> 'test.csv':
+    .name as foo_name
+"""
+        assert parse(src) == [
+            Symbol.OUTPUT,
+            'test.csv',
+            [
+                Symbol.PIPELINE,
+                [Symbol('df')],
+                [
+                    [Symbol.ALIAS, [Symbol.COLUMN, 'name'], Symbol('foo_name')]
+                ]
+            ]
+        ]
+
+    def test_output_stmt_single_line_for_pipeline(self):
+        src = """ pipeline(df) -> 'test.csv': .name as foo_name """
+        assert parse(src) == [
+            Symbol.OUTPUT,
+            'test.csv',
+            [
+                Symbol.PIPELINE,
+                [Symbol('df')],
+                [
+                    [Symbol.ALIAS, [Symbol.COLUMN, 'name'], Symbol('foo_name')]
+                ]
+            ]
+        ]
