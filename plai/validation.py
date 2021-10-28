16e797266130084909d76394d232d8a3b022d35d
Matheus Batista <matheusbattista@hotmail.com>
2021-10-28 17:15:47 -0300

    Implement basic validation return errors

--- plai/validation.py
+++ plai/validation.py
@@ -1,13 +1,23 @@
 import pandas.api.types as ptypes
 
 VALIDATORS = {
-    str: ptypes.is_object_dtype,
-    int: ptypes.is_integer_dtype,
-    float: ptypes.is_float_dtype,
+    'str': ptypes.is_object_dtype,
+    'int': ptypes.is_integer_dtype,
+    'float': ptypes.is_float_dtype,
     'datetime64': ptypes.is_datetime64_dtype
 }
 
 
 def validate_schema(dataframe, schema):
-    validations = [VALIDATORS[dtype](dataframe[col]) for col, dtype in schema.items()]
-    return all(validations)
+    errors = []
+
+    for col, dtype in schema.items():
+        valid = VALIDATORS[dtype](dataframe[col])
+        if not valid:
+            col_dtype = dataframe[col].dtype
+            errors.append({col: 'expected type `{}` got `{}`'.format(dtype, col_dtype)})
+
+    if errors:
+        return {'errors': errors}
+
+    return {}
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,10 +2,26 @@ from plai.validation import validate_schema
 
 
 class TestSchemaValidation:
-    def test_valid_dataframe_schema(self, dataframe):
-        schema = {'name': str, 'number': int, 'floats': float, 'dates': 'datetime64'}
-        assert validate_schema(dataframe, schema) is True
+    def test_valid_dataframe_dtypes(self, dataframe):
+        schema = {
+            'name': 'str',
+            'number': 'int',
+            'floats': 'float',
+            'dates': 'datetime64'
+        }
+        assert validate_schema(dataframe, schema) == {}
 
-    def test_invalid_dataframe_schema(self, dataframe):
-        schema = {'name': str, 'number': float, 'floats': int, 'dates': 'datetime64'}
-        assert validate_schema(dataframe, schema) is False
+    def test_invalid_dataframe_dtypes(self, dataframe):
+        schema = {
+            'name': 'str',
+            'number': 'float',
+            'floats': 'int',
+            'dates': 'datetime64'
+        }
+        expected_errors = {
+            'errors': [
+                {'number': 'expected type `float` got `int64`'},
+                {'floats': 'expected type `int` got `float64`'}
+            ]
+        }
+        assert validate_schema(dataframe, schema) == expected_errors
